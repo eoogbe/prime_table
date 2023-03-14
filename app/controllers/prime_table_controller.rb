f95bad7bc8555b1c45ce12dffed52043f04f512c
Eva Ogbe <eoogbe@gmail.com>
2023-03-14 06:24:56 -0400

    Pass primes directly into view

--- app/controllers/prime_table_controller.rb
+++ app/controllers/prime_table_controller.rb
@@ -4,8 +4,8 @@
 # The controller for the prime_table routes
 class PrimeTableController < ApplicationController
   def show
-    generator = PrimeTableGenerator.new(params[:n].to_i)
-    @prime_table = generator.generate_multiplication_table
+    @primes = PrimeTableGenerator.generate_n_primes(params[:n].to_i)
+    @primes.unshift 1
   end
 
   def new; end
--- spec/views/prime_table/show.html.erb_spec.rb
+++ spec/views/prime_table/show.html.erb_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe 'prime_table/show.html.erb' do
   it 'displays the primes as headers' do
-    assign(:prime_table, [[1, 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]])
+    assign(:primes, [1, 2, 3, 5])
 
     render
 
@@ -14,12 +14,20 @@ RSpec.describe 'prime_table/show.html.erb' do
   end
 
   it 'displays multiplication results as table cells' do
-    assign(:prime_table, [[1, 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]])
+    assign(:primes, [1, 2, 3, 5])
 
     render
 
     { '4' => 1, '6' => 2, '9' => 1, '10' => 2, '15' => 2, '25' => 1 }.each do |cell, count|
       assert_select 'td', text: cell, count:
     end
+  end
+
+  it 'does not display 1' do
+    assign(:primes, [1, 2, 3, 5])
+
+    render
+
+    assert_select 'th', text: '1', count: 0
   end
 end
