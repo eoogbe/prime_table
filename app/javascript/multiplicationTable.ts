30adb5d825e4452352a04fe06cf5bdddd99798eb
Eva Ogbe <eoogbe@gmail.com>
2023-03-14 17:09:54 -0400

    Move all generation of table to TS

--- app/javascript/multiplicationTable.ts
+++ app/javascript/multiplicationTable.ts
@@ -1,6 +1,26 @@
 export default (vals: number[]): void => {
-  vals.forEach((n, i) => {
-    const row = document.querySelector(`tbody tr:nth-child(${i + 1})`);
+  const container = document.getElementById('prime-table');
+  const table = document.createElement('table');
+  container?.appendChild(table);
+  const thead = document.createElement('thead');
+  table.appendChild(thead);
+  const headRow = document.createElement('tr');
+  thead.appendChild(headRow);
+  const dummyCell = document.createElement('td');
+  headRow.appendChild(dummyCell);
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+  vals.forEach((n) => {
+    const colHeader = document.createElement('th');
+    colHeader.scope = 'col';
+    colHeader.textContent = `${n}`;
+    headRow.appendChild(colHeader);
+    const row = document.createElement('tr');
+    tbody.appendChild(row);
+    const rowHeader = document.createElement('th');
+    rowHeader.scope = 'row';
+    rowHeader.textContent = `${n}`;
+    row.appendChild(rowHeader);
     vals.forEach((m) => {
       const cell = document.createElement('td');
       cell.textContent = `${n * m}`;
--- spec/javascript/multiplicationTable.spec.ts
+++ spec/javascript/multiplicationTable.spec.ts
@@ -2,20 +2,28 @@ import insertTable from '@app/multiplicationTable';
 
 describe('multiplicationTable', () => {
   describe('default', () => {
+    it('creates a table', () => {
+      document.body.innerHTML = `<div id="prime-table"></div>`;
+
+      insertTable([2, 3, 5]);
+
+      expect(document.getElementsByTagName('table')).toHaveLength(1);
+    });
+
+    it('adds values as headers', () => {
+      document.body.innerHTML = `<div id="prime-table"></div>`;
+
+      insertTable([2, 3, 5]);
+
+      const result = Array.from(document.querySelectorAll('th')).map(
+        (node) => node.textContent
+      );
+
+      expect(result).toEqual(['2', '3', '5', '2', '3', '5']);
+    });
+
     it('adds multiples of the values to the DOM', () => {
-      document.body.innerHTML = `<table>
-        <tbody>
-          <tr>
-            <th scope="row">2</th>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-          </tr>
-        </tbody>
-      </table>`;
+      document.body.innerHTML = `<div id="prime-table"></div>`;
 
       insertTable([2, 3, 5]);
 
@@ -24,6 +32,7 @@ describe('multiplicationTable', () => {
       );
 
       expect(result).toEqual([
+        '',
         '4',
         '6',
         '10',
