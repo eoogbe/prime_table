79e5dc28725b6029980898f6d4881ae424565d86
Eva Ogbe <eoogbe@gmail.com>
2023-03-14 04:23:03 -0400

    Add system test

---
+++ spec/system/prime_tables_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'PrimeTables' do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'generates a multiplication table for prime numbers' do
+    visit root_path
+
+    fill_in 'Number:', with: '3'
+    click_button 'Generate'
+
+    expect(page).to have_content('25')
+  end
+end
