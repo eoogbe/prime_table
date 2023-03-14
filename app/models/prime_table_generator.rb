4230a3e605e0df28dc618d91ba2b8e52ff2da04c
Eva Ogbe <eoogbe@gmail.com>
2023-03-14 06:30:04 -0400

    Remove multiplication table generator

--- app/models/prime_table_generator.rb
+++ app/models/prime_table_generator.rb
@@ -2,33 +2,15 @@
 
 ##
 # A generator for a multiplication table for prime numbers.
-class PrimeTableGenerator
-  ##
-  # Creates a new generator.
-  #
-  # @param [Integer] n the number of prime numbers in each row and column header of the table
-  def initialize(n)
-    @n = n
-  end
-
-  ##
-  # Generates the prime multiplication table.
-  def generate_multiplication_table
-    values = self.class.generate_n_primes(n)
-    values.unshift 1
-    values.map do |i|
-      values.map do |j|
-        i * j
-      end
-    end
-  end
+module PrimeTableGenerator
+  extend self
 
   ##
   # Calculates primes up to +n+ using the Sieve of Eratosthenes.
   #
   # @param [Integer] n the size of the array
   # @return [Array<Boolean>] for any index k > 2, if index k is +true+ then k is a prime number
-  def self.mark_primes_up_to_n(n)
+  def mark_primes_up_to_n(n)
     result = [true] * (n + 1)
 
     (2..n).each do |i|
@@ -44,12 +26,14 @@ class PrimeTableGenerator
     result
   end
 
-  def self.generate_n_primes(n)
+  def generate_n_primes(n)
     primes = calc_primes(n)
     filter_primes(primes, n)
   end
 
-  def self.calc_primes(n)
+  private
+
+  def calc_primes(n)
     prime_count = 0
     multiplier = 2
     cur = n * multiplier
@@ -62,15 +46,11 @@ class PrimeTableGenerator
     primes
   end
 
-  def self.filter_primes(primes, n)
+  def filter_primes(primes, n)
     values = []
     (2...primes.size).each do |i|
       values << i if primes[i]
       return values if values.size == n
     end
   end
-
-  private
-
-  attr_reader :n
 end
--- spec/models/prime_table_generator_spec.rb
+++ spec/models/prime_table_generator_spec.rb
@@ -7,50 +7,6 @@ RSpec.describe PrimeTableGenerator do
     stub_const('G', PropCheck::Generators)
   end
 
-  describe '#generate_multiplication_table' do
-    it 'has size n + 1' do
-      PropCheck.forall(G.positive_integer) do |n|
-        generator = described_class.new(n)
-
-        result = generator.generate_multiplication_table
-
-        expect(result.size).to eq(n + 1)
-      end
-    end
-
-    it 'has elements of size n + 1' do
-      PropCheck.forall(G.positive_integer) do |n|
-        generator = described_class.new(n)
-
-        result = generator.generate_multiplication_table
-
-        expect(result).to all(have_attributes(size: n + 1))
-      end
-    end
-
-    it 'each cell is a mulitple of the first row and column' do
-      PropCheck.forall(G.positive_integer) do |n|
-        generator = described_class.new(n)
-
-        result = generator.generate_multiplication_table
-
-        (1...result.size).each do |i|
-          (1...result[i].size).each do |j|
-            expect(result[i][j] / result[i][0]).to eq(result[0][j])
-          end
-        end
-      end
-    end
-
-    it 'has row headers of first n prime numbers' do
-      generator = described_class.new(3)
-
-      result = generator.generate_multiplication_table
-
-      expect(result[0]).to eq([1, 2, 3, 5])
-    end
-  end
-
   describe '.mark_primes_up_to_n' do
     it 'returns an array marking the primes up to n as true' do
       result = described_class.mark_primes_up_to_n(7)
