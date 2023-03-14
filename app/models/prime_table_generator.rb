4b5dbc91129a463668e7d97acc40ca34692ebcc4
Eva Ogbe <eoogbe@gmail.com>
2023-03-14 01:17:50 -0400

    Add doc comments

--- app/models/prime_table_generator.rb
+++ app/models/prime_table_generator.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 ##
-# Generates a multiplication table for prime numbers.
+# A generator for a multiplication table for prime numbers.
 class PrimeTableGenerator
   # rubocop:disable Naming/MethodParameterName
+  ##
+  # Creates a new generator.
+  #
+  # @param [Integer] n the size of the table
   def initialize(n)
     # rubocop:enable Naming/MethodParameterName
     @n = n
   end
 
+  ##
+  # Generates the prime multiplication table.
   def generate
     Array.new(@n + 1, Array.new(@n + 1, 1))
   end
