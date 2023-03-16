70fd25aee5f576eb980e402e063153e5b1c2bf70
Eva Ogbe <eoogbe@gmail.com>
2023-03-16 02:24:50 -0400

    Factor out helper method for between

--- app/models/prime.rb
+++ app/models/prime.rb
@@ -3,19 +3,27 @@
 ##
 # Represents the nth prime number.
 class Prime < ApplicationRecord
-  def self.between(range)
-    result = where(n: range).order(:n)
-    return result if result.size == range.size
+  class << self
+    def between(range)
+      result = where(n: range).order(:n)
+      return result if result.size == range.size
 
-    max = range.max
-    min = result.size + range.min - 1
-    addl = PrimeGenerator.generate_n_primes(max)[min...max].each_with_index.map do |prime, i|
-      { n: i + min + 1, prime: }
+      max = range.max
+      min = result.size + range.min - 1
+      generate_and_insert(min, max)
+
+      result
     end
-    # rubocop:disable Rails/SkipsModelValidations
-    Prime.insert_all(addl)
-    # rubocop:enable Rails/SkipsModelValidations
 
-    result
+    private
+
+    def generate_and_insert(min, max)
+      addl = PrimeGenerator.generate_n_primes(max)[min...max].each_with_index.map do |prime, i|
+        { n: i + min + 1, prime: }
+      end
+      # rubocop:disable Rails/SkipsModelValidations
+      Prime.insert_all(addl)
+      # rubocop:enable Rails/SkipsModelValidations
+    end
   end
 end
